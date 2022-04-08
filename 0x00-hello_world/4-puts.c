d86467c356d2f794fdceac822a6aff6cda8a7fd0
gabremlee <gabriel.remilekun@gmail.com>
2022-04-07 17:25:12 -0700

    first hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,8 @@
 
 /**
  *main - 4-puts.c is used to output a string
- * accepts no input as its not a function 
- * Return: 0
+ *accepts no input as its not a function 
+ *Return: 0
  */
 
 int main(void)
