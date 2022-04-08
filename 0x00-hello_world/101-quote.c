a3fb61ae802ce37a07cdfd31db1361a26245ab8f
gabremlee <gabriel.remilekun@gmail.com>
2022-04-07 18:49:27 -0700

    that piece of art

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - program to print to stderr starts here
+ *Return: 0
+ */
+
+int main(void)
+{
+	fprintf(stderr, "%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
+}
