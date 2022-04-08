1b46e4c238bb98bcd697a153e3aff746cde6c851
gabremlee <gabriel.remilekun@gmail.com>
2022-04-07 17:18:35 -0700

    first hello

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * 4-puts.c is used to output a string
+ * accepts no input as its not a function 
+ * returns 0
+ */
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return(0);
+}
