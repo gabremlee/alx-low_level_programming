58ff1e3f5f419fb150bb48cf1cbf3bf35e5bd560
gabremlee <gabriel.remilekun@gmail.com>
2022-04-08 14:04:45 -0700

    remover

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - program start print low but remove 2
+ *Return: 0
+ */
+
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		if ((alpha != 'q') && (alpha != 'e'))
+		{
+			putchar(alpha);
+		}
+		alpha++;
+	}
+	putchar('\n');
+
+	return (0);
+}
