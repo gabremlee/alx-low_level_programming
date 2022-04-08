547d6eee53b9a30f8da296b85ec0191b80dd21ba
gabremlee <gabriel.remilekun@gmail.com>
2022-04-07 18:21:48 -0700

    size of all palava

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
 
 int main(void)
 {
-	printf("size of a char: %d\n byte(s)",sizeof(char));
-	printf("size of a int: %d\n byte(s)",sizeof(int));
-	printf("size of a long int: %d\n byte(s)",sizeof(long int));
-	printf("size of a long long: %d\n byte(s)",sizeof(long long));
-	printf("size of a float: %d\n byte(s)",sizeof(float));
+	printf("size of a char: %d byte(s)\n", sizeof(char));
+	printf("size of a int: %d byte(s)\n", sizeof(int));
+	printf("size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("size of a long long: %d byte(s)\n", sizeof(long long));
+	printf("size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
