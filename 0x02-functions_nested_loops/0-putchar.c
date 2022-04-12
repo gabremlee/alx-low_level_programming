63f912e117cde2ae50a75691d1b153843d6fa09b
gabremlee <gabriel.remilekun@gmail.com>
2022-04-12 14:09:30 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *program starts
+ *returns 0
+ */
+
+int main()
+{
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+
+	return (0);
+}
