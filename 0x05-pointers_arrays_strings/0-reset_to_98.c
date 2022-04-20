ae52c3d162a2addb6a5386403936b23b07799b04
gabremlee <gabriel.remilekun@gmail.com>
2022-04-20 05:41:45 -0700

    Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - function to update the value a pointer points to
+ *@*n - takes in a pointer n for input
+ *Return: no return value
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ *reset_to_98 - function to update the value a pointer points to
+ *@*n - takes in a pointer n for input
+ *Return: no return value
+ */
+
+void reset_to_98(int *n);
+
+#endif
