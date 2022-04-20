239fe485c1101a8d136daec559b2d96973e5cc88
gabremlee <gabriel.remilekun@gmail.com>
2022-04-20 16:24:13 -0700

    updated

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -24,6 +24,15 @@ int _strlen(char *s)
 	return (b);
 }
 
+/**
+ * _strcpy - copies the string pointed to by src,
+ * including the terminating null byte (\0),
+ * to the buffer pointed to by dest
+ * @dest: copy source to this buffer
+ * @src: this is the source to copy
+ * Return: copy of original source
+ */
+
 char *_strcpy(char *dest, char *src)
 {
 	int i;
