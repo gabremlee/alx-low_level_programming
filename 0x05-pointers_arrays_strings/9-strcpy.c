974b5da9d1a9e3c6911f554d4177138a4c2b1fa1
gabremlee <gabriel.remilekun@gmail.com>
2022-04-20 16:22:43 -0700

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ *_strlen - function to get length of given string
+ *@s: string to process
+ *Return: no return value
+ */
+
+int _strlen(char *s)
+{
+	int b = 0, c;
+
+	for (c = 0 ; c < 1000 ; c++)
+	{
+		if (*(s + 0) >= 0 && *(s + c))
+		{
+			b++;
+		}
+		else
+		{
+			break;
+		}
+	}
+	return (b);
+}
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; i <= _strlen(src); i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
