eb8cf9b5ed30e06e00f6add82bcbc12c9c6b7e6f
gabremlee <gabriel.remilekun@gmail.com>
2022-04-20 14:33:42 -0700

    updated

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -25,6 +25,29 @@ int _strlen(char *s)
 }
 
 /**
+ *_puts - function to print strings to standard output
+ *@str: string to process of type char
+ *Return: no return value
+ */
+
+void _puts(char *str, char pp[])
+{
+	int a = 0;
+
+	for (a = 0 ; a < 1000 ; a++)
+	{
+		if (*(str + a) >= 0 && *(str + a))
+		{
+			pp[a] = (*(str + a));
+		}
+		else
+		{
+			break;
+		}
+	}
+}
+
+/**
  *rev_string - function to print string in reverse
  *@s: pointer of type char
  *Return: no return value
@@ -36,11 +59,13 @@ void rev_string(char *s)
 
 	int b, d = 0;
 
-	char c[a] = *s;
+	char pp[a];
+
+	_puts(s, pp);
 
 	for (b = a - 1 ; b >= 0 ; b--)
 	{
-	 	*(s + d) = c[b];
+	 	*(s + d) = pp[b];
 		d++
 	}
 	_putchar('\n');
