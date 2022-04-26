f41782f4db3f3d9d9d640737efcbab04c4e52f23
gabremlee <gabriel.remilekun@gmail.com>
2022-04-26 15:33:34 -0700

    filler

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,11 +10,11 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int a;
+	int a;
 
-	for (int a = 0; a < n ; a++)
+	for (a = 0; a < n ; a++)
 	{
-		s[a] = b;
+		*(s + a) = b;
 	}
 
 	return (s);
