014309ffaf6faa99cb71d95f5e0cd9e9c925967f
gabremlee <gabriel.remilekun@gmail.com>
2022-04-26 15:20:58 -0700

    memory filler 1

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_memset - a function to fill memory
+ *@s: pointer to fill up
+ *@b: constant byte used to fill up s
+ *@n: specify the amount of byte to fill up
+ *Return: pointer s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int a;
+
+	for (int a = 0; a < n ; a++)
+	{
+		*(s + a) = b;
+	}
+
+	return (s);
+}
