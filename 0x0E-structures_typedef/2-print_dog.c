8bcdc20b9e9ae8769bfe5bfa31f576eeaab95cdc
gabremlee <gabriel.remilekun@gmail.com>
2022-05-11 16:08:25 -0700

    print dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,10 +10,13 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
+
 	if (d->name == NULL)
 		d->name = "(nil)";
+
 	if (d->age <= 0)
 		printf("Age: (nil)\n");
+
 	if (d->owner == NULL)
 		d->owner = "(nil)";
 
