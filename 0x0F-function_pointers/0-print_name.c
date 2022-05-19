c6aeee2304d0712f8644fdb4acac348657408a3e
gabremlee <gabriel.remilekun@gmail.com>
2022-05-19 15:33:32 -0700

    updated

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -29,6 +29,6 @@ void print_name_uppercase(char *name);
 
 void print_name(char *name, void (*f)(char *))
 {
-	if(!(name == NULL) && !(f == NULL))
+	if (!(name == NULL) && !(f == NULL))
 		f(name);
 }
