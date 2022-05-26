330ef8bcffd834072a1bdaa57a43af2a0c17b5a1
gabremlee <gabriel.remilekun@gmail.com>
2022-05-26 15:21:02 -0700

    sum it all

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * sum_listint - Total all the data of a listint_t list
+ * @head: Pointer to the head node
+ *
+ * Return: The computed Total
+ */
+
+int Total_listint(listint_t *head)
+{
+	int Total = 0;
+	
+	while (head != NULL)
+	{
+		Total += head->n;
+		head = head->next;
+	}
+
+	return (Total);
+}
