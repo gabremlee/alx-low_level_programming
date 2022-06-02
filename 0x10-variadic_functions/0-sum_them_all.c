23cddbf5a30d01f180da0a36dc7617835fddd358
gabremlee <gabriel.remilekun@gmail.com>
2022-06-02 14:40:33 -0700

    sum them all

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ *sum_them_all - a program to calculate variable sum
+ *@n: first value of the variable arguement
+ *@...: other values
+ *Return: total sum on success and 0 for failure
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	if (n == 0)
+		return 0;
+	va_list elem;
+	int i, sum = 0;
+
+	va_start(elem, n);
+
+	for(i = n ; i >= 0 ; i = va_arg(elem, int)
+		
+		sum += i;
+
+	va_end(elem);
+
+	return sum;
+}
