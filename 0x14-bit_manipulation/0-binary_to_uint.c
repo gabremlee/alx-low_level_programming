3216772610c95f425de0565b3d000bca44539a74
gabremlee <gabriel.remilekun@gmail.com>
2022-08-31 06:07:18 -0700

    updated

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,116 +1,37 @@
-#include <stddef.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
-#include <stdbool.h>
 
 /**
- *check - function to check for characters other than 1 or 0
- *@c: string to search from
- *Return: True if only 1 and 0 present else false
- **/
-
-bool check( const char *c)
-{
-        while( *c != '\0')
-        {
-                if( *c != '1')
-                {
-                        if( *c != '0')
-                        {
-                                return 0;
-                        }
-                }
-                *c++;
-        }
-
-        return 1;
-}
-
-/**
- *len - function to get length of string
- *@p: string to process
- *Return: length of string
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
+ * @b: binary.
+ *
+ * Return: unsigned int.
  */
-
-int len( const char *p)
+unsigned int binary_to_uint(const char *b)
 {
-        int a = 0;
+	unsigned int dec;
+	int len, base;
 
-        while( *p != '\0')
-        {
-                a++;
-                *p++;
-        }
+	if (!b)
+		return (0);
 
-        return a;
-}
+	dec = 0;
 
-/**
- *power - function to calculate power of any number
- *@a: base number to calculate its power
- *@b: number to raise the base to
- *Return: power calculated
- */
+	for (len = 0; b[len] != '\0'; len++)
+		;
 
-int power( int a, int b)
-{
-        int sum;
-        if( b > 0)
-        {
-                sum = a;
-        }else
-        {
-                sum = 1;
-        }
+	for (len--, base = 1; len >= 0; len--, base *= 2)
+	{
+		if (b[len] != '0' && b[len] != '1')
+		{
+			return (0);
+		}
 
-        while( b > 1)
-        {
-                sum *= a;
-                b--;
-        }
+		if (b[len] & 1)
+		{
+			dec += base;
+		}
+	}
 
-        return sum;
-}
-
-/**
- *binary_to_int - function to convert string binary to unsigned int decimal
- *@b: string to process
- *Return: Decimal equivalent of binary
- */
-
-
-unsigned int binary_to_uint( const char *b)
-{
-
-        unsigned int dec = 0;
-
-        if(*b == NULL)
-        {
-                return 0;
-        }
-
-
-        if( !(check(b)) )
-        {
-                return 0;
-        }
-
-
-        int a = len(b)-1;
-
-
-
- 	while( *b != '\0' )
-        {
-                if(*b == '1')
-                {
-                        dec += power(2,a);
-                }
-
-                *b++;
-                a--;
-        }
-
-        return dec;
+	return (dec);
 }
